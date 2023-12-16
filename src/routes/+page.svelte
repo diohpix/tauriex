c6c5223833f0f8b231a0d9d9007ca0fca0ca1ece
diohpix <diohpix@gmail.com>
2023-12-17 01:04:10 +0900

    first

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    let count=0;
+    function gog(){
+        console.log(1)
+        count=count+1
+    }
+</script>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation {count}</p>
+<button on:click={gog}>dowekdowekd</button>
