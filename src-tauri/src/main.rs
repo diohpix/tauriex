7f513bc64d118f862a29d0992f993d45b2686bae
diohpix <diohpix@gmail.com>
2023-12-19 23:29:56 +0900

    xterm

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,4 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use std::collections::HashMap;
 use tauri::Builder;
@@ -19,7 +20,7 @@ impl JexpeState {
         }
     }
 }
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+
 
 
 fn main() {
