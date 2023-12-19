cd37fceedc42388520403a8231f2970071dd0225
이종은 <jongeun-lee@nhn.com>
2023-12-19 16:10:48 +0900

    xterm

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,37 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use std::collections::HashMap;
+use tauri::Builder;
+use tokio::sync::{Mutex};
+
+use crate::pty::PtyProcess;
+
+mod pty;
+mod shell;
+
+pub struct JexpeState {
+    ptys: Mutex<HashMap<String, PtyProcess>>,
+}
+
+impl JexpeState {
+    fn new() -> Self {
+        Self {
+            ptys: Mutex::new(HashMap::new()),
+        }
+    }
+}
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+
 
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+  Builder::default()
+      .manage(JexpeState::new())
+      .invoke_handler(tauri::generate_handler![
+          shell::commands::get_system_shells,
+          pty::commands::spawn_pty,
+          pty::commands::write_pty,
+          pty::commands::resize_pty,
+          pty::commands::kill_pty,
+      ])
+      .run(tauri::generate_context!())
+      .expect("error while running jexpe application");
 }
